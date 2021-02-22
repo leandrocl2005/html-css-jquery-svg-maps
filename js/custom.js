d5fecc2d4f1775ed8545a20d5e935cdaefb1d3f6
leandrocl2005 <leandrocl2005@yahoo.com.br>
2021-02-22 18:05:58 -0300

    refactoring using no js libs

--- js/custom.js
+++ js/custom.js
@@ -1,140 +1,163 @@
 const data = {
   zonaDaMata: {
-    id: "#zona-da-mata--inject-1",
+    id: "zona-da-mata",
     classToAdd: 'importantYellow',
     name: "Zona da Mata",
-    riskClassification: "Onda Amarela",
+    risk: "Onda Amarela",
     defaultColor: '#ffd166',
+    hoverColor: '#ffe1a6',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   trianguloMineiro: {
-    id: "#triangulo-mineiro--inject-1",
+    id: "triangulo-mineiro",
     classToAdd: 'importantGreen',
     name: "Triângulo Mineiro",
-    riskClassification: "Onda Verde",
+    risk: "Onda Verde",
     defaultColor: '#06d6a0',
+    hoverColor: '#35ffbf',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   jequitinhonha: {
-    id: "#jequitinhonha--inject-1",
+    id: "jequitinhonha",
     classToAdd: 'importantRed',
     name: "Jequitinhonha",
-    riskClassification: "Onda Vermelha",
+    risk: "Onda Vermelha",
     defaultColor: '#ef476f',
+    hoverColor: '#ff6489',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   norte: {
-    id: "#norte--inject-1",
+    id: "norte",
     classToAdd: 'importantYellow',
     name: "Norte de Minas",
-    riskClassification: "Onda Amarela",
+    risk: "Onda Amarela",
     defaultColor: '#ffd166',
+    hoverColor: '#ffe1a6',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   centralMineira: {
-    id: "#central-mineira--inject-1",
+    id: "central-mineira",
     classToAdd: 'importantRed',
     name: "Central Mineira",
-    riskClassification: "Onda Vermelha",
+    risk: "Onda Vermelha",
     defaultColor: '#ef476f',
+    hoverColor: '#ff6489',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   sul: {
-    id: "#sul--inject-1",
+    id: "sul",
     classToAdd: 'importantGreen',
     name: 'Sul de Minas',
-    riskClassification: "Onda Verde",
+    risk: "Onda Verde",
     defaultColor: '#06d6a0',
+    hoverColor: '#35ffbf',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   regiaoMetropolitanaDeBH: {
-    id: "#regiao-metropolitana-de-bh--inject-1",
+    id: "regiao-metropolitana-de-bh",
     classToAdd: 'importantRed',
     name: "Metropolitana de BH",
-    riskClassification: "Onda Vermelha",
+    risk: "Onda Vermelha",
     defaultColor: '#ef476f',
+    hoverColor: '#ff6489',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   noroesteDeMinas: {
-    id: "#noroeste-de-minas--inject-1",
+    id: "noroeste-de-minas",
     classToAdd: 'importantYellow',
     name: "Noroeste de Minas",
-    riskClassification: "Onda Amarela",
+    risk: "Onda Amarela",
     defaultColor: '#ffd166',
+    hoverColor: '#ffe1a6',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   valeDoRioDoce: {
-    id: "#vale-do-rio-doce--inject-1",
+    id: "vale-do-rio-doce",
     classToAdd: 'importantRed',
     name: "Vale do Rio Doce",
-    riskClassification: "Onda Vermelha",
+    risk: "Onda Vermelha",
     defaultColor: '#ef476f',
+    hoverColor: '#ff6489',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   valeDoMucuri: {
-    id: "#vale-do-mucuri--inject-1",
+    id: "vale-do-mucuri",
     classToAdd: 'importantYellow',
     name: "Vale do Mucuri",
-    riskClassification: "Onda Amarela",
+    risk: "Onda Amarela",
     defaultColor: '#ffd166',
+    hoverColor: '#ffe1a6',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   camposDasVertentes: {
-    id: "#campos-das-vertentes--inject-1",
+    id: "campos-das-vertentes",
     classToAdd: 'importantYellow',
     name: "Campos das Vertentes",
-    riskClassification: "Onda Amarela",
+    risk: "Onda Amarela",
     defaultColor: '#ffd166',
+    hoverColor: '#ffe1a6',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
   oesteDeMinas: {
-    id: "#oeste-de-minas--inject-1",
+    id: "oeste-de-minas",
     classToAdd: 'importantYellow',
     name: "Oeste de Minas",
-    riskClassification: "Onda Amarela",
+    risk: "Onda Amarela",
     defaultColor: '#ffd166',
+    hoverColor: '#ffe1a6',
     cases: 1500 + Math.floor(20000 * Math.random()),
     deaths: 15 + Math.floor(200 * Math.random())
   },
 }
 
-function main() {
-  Object.keys(data).forEach(key => {
+function changeInfo(key) {
+  document.getElementById('zone-name').innerHTML = data[key].name;
+  document.getElementById('zone-risk').innerHTML = data[key].risk;
+  document.getElementById('zone-risk').style.borderBottom = "2px solid "
+    + data[key].defaultColor;
+  document.getElementById('zone-cases').innerHTML = data[key].cases;
+  document.getElementById('zone-deaths').innerHTML = data[key].deaths;
+}
 
-    const dataZone = data[key];
-    const zone = $(dataZone.id);
-    console.log("here");
+function changeStyle(key) {
 
-    zone.addClass(dataZone.classToAdd);
-    console.log("now here");
-
-    zone.on('click', function () {
-      $('#zone-name').text(dataZone.name);
-      $('#zone-risk')
-        .text(dataZone.riskClassification)
-        .css('borderBottom', `2px solid ${dataZone.defaultColor}`);
-      $('#zone-deaths').text(dataZone.deaths);
-      $('#zone-cases').text(dataZone.cases);
-    })
-  })
 }
 
 
-if (document.readyState == 'complete')
-  main();
+const svg = document.getElementById("mg-map");
 
-$(window).bind("load", main);
+svg.addEventListener("load", function () {
 
+  const svgDoc = svg.contentDocument
 
+  Object.keys(data).forEach(key => {
+    const zone = svgDoc.getElementById(data[key].id);
 
+    zone.style.fill = data[key].defaultColor;
+    zone.style.cursor = 'pointer';
+
+    zone.addEventListener('mouseenter', function () {
+      zone.style.fill = data[key].hoverColor;
+    }, false);
+
+    zone.addEventListener('mouseleave', function () {
+      zone.style.fill = data[key].defaultColor;
+    }, false)
+
+    zone.addEventListener("click", function () {
+      changeInfo(key);
+    }, false);
+  })
+
+}, false);
